13dec0dc81c20b1e6d25d50fe84ea9542b3ee1ca
ryan broman <ryanjbroman@gmail.com>
2021-03-09 10:45:13 -0800

    Player damage log

---
+++ ValheimRP/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace ValheimRP {
+    [HarmonyPatch(typeof(Player), "OnDamaged")]
+    internal class VRPPlayer {
+        [UsedImplicitly]
+        private static bool Prefix(Player __instance, HitData hit) {
+            var attacker = hit.GetAttacker();
+            var attackerName = "enemy or environment";
+            if (attacker)
+                attackerName = attacker.m_name;
+            
+            var damage = hit.GetTotalDamage();
+            
+            Log.LogInfo($"[Hit] {__instance.GetPlayerName()} hit by {attackerName} for {damage}dmg");
+            return true;
+        }
+    }
+}
