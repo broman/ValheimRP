7a5683089b1a277a106a9154a2fbe748a7176a0a
ryan broman <ryanjbroman@gmail.com>
2021-03-08 18:53:32 -0800

    Reformat to correct way

--- ValheimRP/Util/CoroutineExtensions.cs
+++ ValheimRP/Util/CoroutineExtensions.cs
@@ -2,17 +2,13 @@
 using System.Collections;
 using UnityEngine;
 
-namespace ExampleMod.Util
-{
-    internal static class CoroutineExtensions
-    {
-        internal static void DelayedMethod(float seconds, Action method)
-        {
-            ExampleMod.Instance.StartCoroutine(InternalDelayedMethod(seconds, method));
+namespace ValheimRP.Util {
+    internal static class CoroutineExtensions {
+        internal static void DelayedMethod(float seconds, Action method) {
+            ValheimRP.Instance.StartCoroutine(InternalDelayedMethod(seconds, method));
         }
 
-        private static IEnumerator InternalDelayedMethod(float seconds, Action method)
-        {
+        private static IEnumerator InternalDelayedMethod(float seconds, Action method) {
             yield return new WaitForSeconds(seconds);
 
             method();
---
+++ ValheimRP/ValheimRP.cs
@@ -0,0 +1,76 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ValheimRP {
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    public class ValheimRP : BaseUnityPlugin {
+        private const string ModGuid = AuthorName + "." + ModName;
+        private const string AuthorName = "ValheimRP";
+        private const string ModName = "ValheimRP";
+        private const string ModVer = "0.0.1";
+
+        internal static Harmony HarmonyInstance;
+
+        internal static ValheimRP Instance { get; private set; }
+
+        /// <summary>
+        ///     Awake is called when the script instance is being loaded.
+        /// </summary>
+        private void Awake() {
+            Instance = this;
+
+            Log.Init(Logger);
+
+            Init();
+        }
+
+        /// <summary>
+        ///     Destroying the attached Behaviour will result in the game or Scene receiving OnDestroy.
+        ///     OnDestroy occurs when a Scene or game ends.
+        ///     It is also called when your mod is unloaded, this is where you do clean up of hooks, harmony patches,
+        ///     loose GameObjects and loose monobehaviours.
+        ///     Loose here refers to gameobjects not attached
+        ///     to the parent BepIn GameObject where your BaseUnityPlugin is attached
+        /// </summary>
+        private void OnDestroy() {
+            Disable();
+        }
+
+        internal static void Init() {
+            EnableMonoModHooks();
+
+            EnableHarmonyPatches();
+        }
+
+        /// <summary>
+        ///     Disable the enabled hooks.
+        /// </summary>
+        private static void Disable() {
+            DisableMonoModHooks();
+
+            DisableHarmonyPatches();
+        }
+
+        private static void EnableMonoModHooks() {
+            On.FejdStartup.Awake += OnFejdStartupAwakeMonoModHookShowcase;
+        }
+
+        private static void DisableMonoModHooks() {
+            On.FejdStartup.Awake -= OnFejdStartupAwakeMonoModHookShowcase;
+        }
+
+        private static void EnableHarmonyPatches() {
+            HarmonyInstance = new Harmony(ModGuid);
+            HarmonyInstance.PatchAll();
+        }
+
+        private static void DisableHarmonyPatches() {
+            HarmonyInstance.UnpatchSelf();
+        }
+
+        private static void OnFejdStartupAwakeMonoModHookShowcase(On.FejdStartup.orig_Awake orig, FejdStartup self) {
+            // calling the original method
+            orig(self);
+        }
+    }
+}
