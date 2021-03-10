3696bba76bfcd45bdf0dbeac8bc2654422866cc9
ryan broman <ryanjbroman@gmail.com>
2021-03-09 19:04:04 -0800

    Update player logger

--- ValheimRP/Player.cs
+++ ValheimRP/Player.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using HarmonyLib;
+﻿using HarmonyLib;
 using JetBrains.Annotations;
 
 namespace ValheimRP {
@@ -13,15 +12,20 @@ namespace ValheimRP {
         /// <returns>true always</returns>
         [UsedImplicitly]
         private static bool Prefix(Player __instance, HitData hit) {
-            var attacker = hit.GetAttacker();
-            var attackerName = "enemy or environment";
-            if (attacker)
-                attackerName = attacker.m_name;
-            
+            var attacker = GetPlayerInfo(hit.GetAttacker());
+
             var damage = hit.GetTotalDamage();
             
-            Log.LogInfo($"[Hit] {__instance.GetPlayerName()} hit by {attackerName} for {damage}dmg");
+            Log.LogInfo($"{GetPlayerInfo(__instance)} hit by {attacker} for {damage}dmg");
             return true;
+        }
+
+        private static string GetPlayerInfo(Character character) {
+            return !character ? "environment" : $"{character.m_name}{character.GetCenterPoint()}";
+        }
+
+        private static string GetPlayerInfo(Player player) {
+            return $"{player.GetPlayerName()}{player.GetHeadPoint()}";
         }
     }
 }
