425ee1804cba06e729958ced9df10fae4a8d2256
ryan broman <ryanjbroman@gmail.com>
2021-03-13 20:21:45 -0800

    Prevent items in wards from grief

---
+++ ValheimRP/WearNTear.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * WearNTear.cs
+ * Created by Ryan Broman on 2021-03-13
+ * ryan@broman.dev
+ */
+
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace ValheimRP {
+    [UsedImplicitly]
+    [HarmonyPatch(typeof(WearNTear))]
+    internal class VRPWearNTear {
+        [HarmonyPatch("RPC_Damage")]
+        private static bool Prefix(WearNTear __instance, long sender, HitData hit) {
+            return !PrivateArea.CheckInPrivateArea(__instance.transform.position, true);
+        }
+    }
+}
